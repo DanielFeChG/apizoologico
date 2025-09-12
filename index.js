c407d775bee28b6236faf1a65e6355e36cf6794d
DanielChavez <danielf.chavezg@konradlorenz.edu.co>
2025-09-11 19:34:26 -0500

    Se agrega el archivo anotaciones, y se agrega nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 
 app.get('/', (req, res) => { //El objeto es app, el metodo (.get) va a hacer referencia a los verbos HTTP, parametros de get ('/', (req, res)),  este primer parametro es la ruta, forma de escribir metodos ()=>{...}  ... es el cuerpo de la funcion 
-    res.send("¡Hola Daniel!") //req: request(cuando se necesita traer alguna informacion necesaria para nuestra peticion)    res: response (respuesta)
+    res.send("¡Hola Daniel Felipe Chavez!") //req: request(cuando se necesita traer alguna informacion necesaria para nuestra peticion)    res: response (respuesta)
 
 })
 
